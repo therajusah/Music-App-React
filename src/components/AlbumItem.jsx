10049b2b7b169e42a318b646db155db23790d216
therajusah <workwithrajukumar@gmail.com>
2024-02-02 00:17:25 +0530

    Album Done

---
+++ src/components/AlbumItem.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+const AlbumItems = ({ name, artists, id, image }) => {
+  return (
+    <Link
+      to={`/album/${id}`}
+      className="w-[160px] max-h[220px] overflow-y-clip flex flex-col justify-center items-center gap-3 rounded-lg"
+    >
+      <img src={image[2].Link} alt="" className="rounded-lg" />
+      <div className="text-[13px] w-full flex flex-col justify-center items-center">
+        <span className="text-gray-600 font-semibold overflow-x-clip">
+          {name}
+        </span>
+        <p className="text-gray-500 font-thin">
+          {artists.map((artist) => artist.name).join(",")}
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default AlbumItems;
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,26 +1,28 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import AlbumItem from "./AlbumItem";
 const MainSection = () => {
-    
-const getHomePageData = async () => {
+  const [ albums, setAlbums] = useState([]);
+  const [ trending, setTrending] = useState([]);
 
-    const [albums, setAlbums] = useState([]);
-    const [ trending, setTrending] = useState([]);
+
+const getHomePageData = async () => {
     const res = await axios.get("https://saavn.me/modules?language=hindi");
     const {data} = res.data;
-    console.log(data);
     setAlbums(data.albums);
     setTrending(data.trending);
 };
+
 useEffect(() => {
    getHomePageData();
 }, []);
 
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  return <div>
+      {
+        albums?.map((album) => <AlbumItem key={album.id} {...album}/>)
+      }
+    </div>;
+ 
+};
 
 export default MainSection;
